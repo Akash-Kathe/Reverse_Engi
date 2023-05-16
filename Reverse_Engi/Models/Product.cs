31a821ea9482ddf5527c8536af52a48030ee4858
Admin <Admin@DESKTOP-TVJAM8Q>
2023-05-16 13:49:52 +0530

    Add project files.

---
+++ Reverse_Engi/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Reverse_Engi.Models;
+
+[Table("Product")]
+public partial class Product
+{
+    [Key]
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    [Column("Price ")]
+    public double? Price { get; set; }
+
+    public int? Qty { get; set; }
+}
